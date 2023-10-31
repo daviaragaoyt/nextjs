1718c5d8c9005f6c772536cae935f7185ddb71c2
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-10-31 18:13:04 -0300

    usando useRouter

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,27 +1,23 @@
-import Image from "next/image";
-import logo from '../../../public/logo3.png';
-import { Box } from "@chakra-ui/react";
+import Image from 'next/image'
+import logo from '../../../public/logo3.png'
+import { Box } from '@chakra-ui/react'
 
 const Navbar = () => {
-
-    return (
-    <Box className="header-menu">
-        <nav>
-            <Image src={logo} className="logo3" alt="logo3" width={100}/>
+   return (
+      <Box className="header-menu">
+         <nav>
+            <Image src={logo} className="logo3" alt="logo3" width={100} />
             <ul className="lista-menu">
-                        <li>
-                            <a href="#Horarios">Horarios</a>
-                        </li>
-                    <li>
-                        <a href="..#Informações">Avisos</a>
-                    </li>
-                    <li>
-                        <a href="#contato">Contato</a>
-                    </li>
-                </ul>
-        </nav>
-    </Box>
-    )
+               <li>
+                  <a href="#Horarios">Horarios</a>
+               </li>
+               <li>
+                  <a href="#contato">Contato</a>
+               </li>
+            </ul>
+         </nav>
+      </Box>
+   )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import { Container, Box } from '@chakra-ui/react'
+import { Container, Box, Button } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import logo from '../../public/Horizontal-Colorida.png'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+   const router = useRouter()
    const urlDestino = 'https://nextjs-ebon-delta-53.vercel.app/formulario'
 
+   const telaCadastro = () => {
+      router.push('/formulario')
+   }
+
+   function teste() {}
    return (
       <Box className="body">
          <Navbar />
@@ -28,11 +35,15 @@ const Home: NextPage = () => {
                </div>
                <br />
                <div className="horarios">
-                  <h1 className="lista-descricao">
-                     <strong>
-                        Aqui está nosso horario de atendimento por ordem de
-                        chegada<br/> (no caso por ordem de cadastro dos formularios):
-                     </strong>
+                  <div className="lista-descricao">
+                     <h1>
+                        <strong>
+                           Aqui está nosso horario de atendimento por ordem de
+                           chegada
+                           <br /> (no caso por ordem de cadastro dos
+                           formularios):
+                        </strong>
+                     </h1>
                      <h3>
                         <strong>Segunda a sexta:</strong>
                      </h3>
@@ -44,7 +55,7 @@ const Home: NextPage = () => {
                            <strong>E das 14:00 as 21:00</strong>
                         </li>
                      </ul>
-                  </h1>
+                  </div>
                </div>
             </article>
          </section>
@@ -60,7 +71,7 @@ const Home: NextPage = () => {
                         <h1 className="lista-titulo">
                            Alguns avisos importantes sobre o cadastro:
                         </h1>
-                        <h1 className="lista-descricao">
+                        <div className="lista-descricao">
                            As cirurgias são marcadas de acordo om a demanda de
                            pacientes para as aulas praticas realizadas pelos
                            alunos de medicina veterinária da Uniceplac.
@@ -68,31 +79,39 @@ const Home: NextPage = () => {
                            Caso você e seu pet sejam escolidos, entraremos em
                            contato apenas 3 vezes, por meio do seu numero.
                            <br />
-                           <strong>
-                              As consultas não são gratuitas,para mais
-                              informações contate-nos.
-                           </strong>
+                           <h1>
+                              <strong>
+                                 As consultas não são gratuitas,para mais
+                                 informações contate-nos.
+                              </strong>
+                           </h1>
                            <br />
                            <br />
                            <h1 className="Alert">Atenção:</h1>
                            <h1 className="lista-descricao">
                               <br />
-                              <strong>Será feita 2 a 3 tentativas de contato,caso não
-                              tenhamos retorno, o paciente será retirado da
-                              lista de espera</strong>
+                              <strong>
+                                 Será feita 2 a 3 tentativas de contato,caso não
+                                 tenhamos retorno, o paciente será retirado da
+                                 lista de espera
+                              </strong>
                               <br />
                            </h1>
                            <br />
-                        </h1>
+                        </div>
                      </li>
-                     <a
-                        className="link-cad"
-                        href={urlDestino}
-                        target="_blank"
-                        rel="noopener noreferrer"
+                     <Button
+                        color="#fff"
+                        bgColor="#d68749"
+                        onClick={telaCadastro}
+                        border="5px solid #d68749"
+                        _hover={{
+                           background: '#3b7355',
+                           borderColor: '#3b7355',
+                        }}
                      >
-                        <button>Fazer Cadastro</button>
-                     </a>
+                        Fazer Cadastro
+                     </Button>
                   </ul>
                </div>
             </article>
@@ -103,6 +122,7 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
 function setIsClient(arg0: boolean) {
    throw new Error('Function not implemented.')
 }
