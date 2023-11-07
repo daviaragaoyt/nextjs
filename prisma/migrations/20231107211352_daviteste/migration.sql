fd42aa61a17c47e4d6b871eee06b58689435bdc4
Herbert Valadares <herberticleber@gmail.com>
2023-11-07 18:14:14 -0300

    update

---
+++ prisma/migrations/20231107211352_daviteste/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Todo";
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Formulario" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "sobrenome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "nomePet" TEXT NOT NULL,
+    "especie" TEXT NOT NULL,
+    "raca" TEXT NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Formulario_pkey" PRIMARY KEY ("id")
+);
