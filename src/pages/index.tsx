2d48e6ae06bf96ec0a24e8b747ff9af4ecf5d001
DAVI <daviaragaoyt@gmail.com>
2023-11-07 15:30:52 -0300

    NavBar atualizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
-import { Container, Box, Button } from '@chakra-ui/react'
+import { Container, Box } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import logo from '../../public/Horizontal-Colorida.png'
-import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
-<<<<<<< HEAD
    const urlDestino = './formulario'
-=======
-   const router = useRouter()
-   const urlDestino = 'https://nextjs-ebon-delta-53.vercel.app/formulario'
->>>>>>> 32e786eb29a4dd0033a51a1558b9c6774df7defe
 
-   const telaCadastro = () => {
-      router.push('/formulario')
-   }
-
-   function teste() {}
    return (
       <Box className="body">
          <Navbar />
@@ -38,25 +27,12 @@ const Home: NextPage = () => {
                   Hospital Veterinario da Uniceplac
                </div>
                <br />
-<<<<<<< HEAD
                <div id="#horarios" className="horarios">
                   <h1 className="lista-descricao">
                      <strong>
                         Aqui está nosso horario de atendimento por ordem de
                         chegada<br/> (no caso por ordem de cadastro dos formularios):
                      </strong>
-=======
-               <div className="horarios">
-                  <div className="lista-descricao">
-                     <h1>
-                        <strong>
-                           Aqui está nosso horario de atendimento por ordem de
-                           chegada
-                           <br /> (no caso por ordem de cadastro dos
-                           formularios):
-                        </strong>
-                     </h1>
->>>>>>> 32e786eb29a4dd0033a51a1558b9c6774df7defe
                      <h3>
                         <strong>Segunda a sexta:</strong>
                      </h3>
@@ -68,7 +44,7 @@ const Home: NextPage = () => {
                            <strong>E das 14:00 as 21:00</strong>
                         </li>
                      </ul>
-                  </div>
+                  </h1>
                </div>
             </article>
          </section>
@@ -84,7 +60,7 @@ const Home: NextPage = () => {
                         <h1 className="lista-titulo">
                            Alguns avisos importantes sobre o cadastro:
                         </h1>
-                        <div className="lista-descricao">
+                        <h1 className="lista-descricao">
                            As cirurgias são marcadas de acordo om a demanda de
                            pacientes para as aulas praticas realizadas pelos
                            alunos de medicina veterinária da Uniceplac.
@@ -92,39 +68,31 @@ const Home: NextPage = () => {
                            Caso você e seu pet sejam escolidos, entraremos em
                            contato apenas 3 vezes, por meio do seu numero.
                            <br />
-                           <h1>
-                              <strong>
-                                 As consultas não são gratuitas,para mais
-                                 informações contate-nos.
-                              </strong>
-                           </h1>
+                           <strong>
+                              As consultas não são gratuitas,para mais
+                              informações contate-nos.
+                           </strong>
                            <br />
                            <br />
                            <h1 className="Alert">Atenção:</h1>
                            <h1 className="lista-descricao">
                               <br />
-                              <strong>
-                                 Será feita 2 a 3 tentativas de contato,caso não
-                                 tenhamos retorno, o paciente será retirado da
-                                 lista de espera
-                              </strong>
+                              <strong>Será feita 2 a 3 tentativas de contato,caso não
+                              tenhamos retorno, o paciente será retirado da
+                              lista de espera</strong>
                               <br />
                            </h1>
                            <br />
-                        </div>
+                        </h1>
                      </li>
-                     <Button
-                        color="#fff"
-                        bgColor="#d68749"
-                        onClick={telaCadastro}
-                        border="5px solid #d68749"
-                        _hover={{
-                           background: '#3b7355',
-                           borderColor: '#3b7355',
-                        }}
+                     <a
+                        className="link-cad"
+                        href={urlDestino}
+                        target="_blank"
+                        rel="noopener noreferrer"
                      >
-                        Fazer Cadastro
-                     </Button>
+                        <button>Fazer Cadastro</button>
+                     </a>
                   </ul>
                </div>
             </article>
@@ -135,7 +103,6 @@ const Home: NextPage = () => {
 }
 
 export default Home
-
 function setIsClient(arg0: boolean) {
    throw new Error('Function not implemented.')
 }
